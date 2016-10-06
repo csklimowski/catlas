aac2cd7004b7548b84f6b3cede71add1d551c896
csenderling <csenderling@gmail.com>
2016-10-05 21:27:05 -0700

    Converged TextView and HorizontalScrollView into the same view

--- app/src/main/java/teamchrisplus/catlas/MainActivity.java
+++ app/src/main/java/teamchrisplus/catlas/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
