2a8b2757977ee7ae734d2756085250fd34a1c376
Chris Klimowski <csklimowski@gmail.com>
2016-11-01 01:27:13 -0700

    searching for rooms on the current floor works

--- app/src/main/java/teamchrisplus/catlas/MainActivity.java
+++ app/src/main/java/teamchrisplus/catlas/MainActivity.java
@@ -4,17 +4,18 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.content.Intent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.PopupWindow;
 
@@ -127,6 +128,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         TextView myTextView = (TextView) findViewById(R.id.my_textView);
         myTextView.setText(floor.getName());
+
+        handleIntent(getIntent());
     }
 
     @Override
@@ -244,6 +247,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.room_list_menu, menu);
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_rooms).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -253,9 +262,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         switch (item.getItemId()) {
             case R.id.menu_room_list:
                 startActivity(new Intent(MainActivity.this, RoomDBActivity.class));
-                break;
-            case R.id.search_rooms:
-                // search for rooms
                 break;
         }
 
@@ -274,11 +280,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         // Set up the layour and initialize the PopupWindow
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = inflater.inflate(R.layout.room_popup, (ViewGroup) findViewById(R.id.room_popup));
         PopupWindow pw = new PopupWindow(
-                layout,
-                600,
-                600,
+                inflater.inflate(R.layout.room_popup, null, false),
+                300,
+                300,
                 true
         );
 
@@ -286,7 +291,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         ((TextView) pw.getContentView().findViewById(R.id.popup_text_view)).setText(room.getPopupInfo());
 
         // Add Button and listener
-        Button button = (Button) layout.findViewById(R.id.route_button);
+        Button button = (Button) pw.getContentView().findViewById(R.id.route_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -295,8 +300,35 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         });
 
         // Display the window @ Room's center
-        pw.showAtLocation(layout, Gravity.CENTER, x, y);
+        pw.showAtLocation(hView, Gravity.CENTER, x, y);
         pw.update(x - hView.getScrollX(), y, 300, 300);
     }
 
+    /*
+    * If the requested room exists, finds it on the floor and selects it.
+    *
+    */
+    private void searchRooms(String query) {
+        System.out.println("test");
+        query = query.toLowerCase();
+        for(final DBRoom room : floor.getRooms()) {
+            if (room.get_name().toLowerCase().contains(query)) {
+                showPopupWindow(room);
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchRooms(query);
+        }
+    }
 }
