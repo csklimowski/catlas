c891aa1da0ed4f02475e945cf003f7216a65337c
= <=>
2016-11-30 00:27:23 -0700

    added front screen

---
+++ app/src/main/java/teamchrisplus/catlas/FrontScreenActivity.java
@@ -0,0 +1,21 @@
+package teamchrisplus.catlas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+/**
+ * Created by Christopher on 11/29/2016.
+ */
+
+public class FrontScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_screen);
+    }
+}
