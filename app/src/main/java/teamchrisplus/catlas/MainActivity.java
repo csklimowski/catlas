90bcd63ee514cea6f10b52813d0974c3bd35792e
= <=>
2016-12-01 05:10:53 -0700

    fixed node collision boxes to be larger

--- app/src/main/java/teamchrisplus/catlas/MainActivity.java
+++ app/src/main/java/teamchrisplus/catlas/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private Floor floor;
     private FloorNode destinationNode;
     private FloorNode sourceNode;
+    private FloorNode selectedNode;
     private ArrayList<Floor> floorList = new ArrayList<Floor>();
     private DBManager db;
     private int downX = 0;
@@ -71,83 +72,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         spec.setIndicator("Floor 9");
         host.addTab(spec);
 
-        LoadFloors load = new LoadFloors();
-        //floor = load.getFloor("GS Floor 9");
-
         db = new DBManager(this);
         ArrayList<DBRoom> rooms = db.getAllRooms();
         db.close();
 
-        FloorGraph graph = new FloorGraph();
-
-        FloorNode node1 = new FloorNode(2200, 1050, new Rect(2060, 812, 2368, 1310));
-        FloorNode node2 = new FloorNode(1730, 1220, new Rect(1555, 1139, 1934, 1310));
-        FloorNode node3 = new FloorNode(750, 1220, new Rect(600, 1139, 916, 1310));
-        FloorNode node4 = new FloorNode(750, 1050, new Rect(600, 971, 916, 1139));
-        FloorNode node5 = new FloorNode(750, 880, new Rect(600, 815, 916, 971));
-
-        FloorNode node6 = new FloorNode(2800, 380, new Rect(2700, 300, 2900, 460));
-        FloorNode node7 = new FloorNode(2400, 380, new Rect(2300, 300, 2500, 460));
-        FloorNode node8 = new FloorNode(2400, 560, new Rect(2300, 480, 2500, 620));
-        FloorNode node9 = new FloorNode(2400, 1380, new Rect(2300, 1300, 2500, 1460));
-        FloorNode node10 = new FloorNode(2200, 1380, new Rect(2100, 1300, 2300, 1460));
-        FloorNode node11 = new FloorNode(1730, 1380, new Rect(1630, 1300, 1830, 1460));
-        FloorNode node12 = new FloorNode(750, 1380, new Rect(650, 1300, 850, 1460));
-        FloorNode node13 = new FloorNode(540, 1380, new Rect(440, 1300, 640, 1460));
-        FloorNode node14 = new FloorNode(540, 1050, new Rect(440, 980, 640, 1130));
-        FloorNode node15 = new FloorNode(540, 880, new Rect(440, 800, 640, 960));
-        FloorNode node16 = new FloorNode(540, 570, new Rect(440, 490, 640, 650));
-        FloorNode node17 = new FloorNode(870, 570, new Rect(770, 490, 970, 650));
-        FloorNode node18 = new FloorNode(870, 440, new Rect(770, 360, 970, 520));
-        FloorNode node19 = new FloorNode(2100, 440, new Rect(2000, 360, 2200, 520));
-        FloorNode node20 = new FloorNode(2100, 560, new Rect(2000, 480, 2200, 620));
-
-        graph.addNode(node1);
-        graph.addNode(node2);
-        graph.addNode(node3);
-        graph.addNode(node4);
-        graph.addNode(node5);
-        graph.addNode(node6);
-        graph.addNode(node7);
-        graph.addNode(node8);
-        graph.addNode(node9);
-        graph.addNode(node10);
-        graph.addNode(node11);
-        graph.addNode(node12);
-        graph.addNode(node13);
-        graph.addNode(node14);
-        graph.addNode(node15);
-        graph.addNode(node16);
-        graph.addNode(node17);
-        graph.addNode(node18);
-        graph.addNode(node19);
-        graph.addNode(node20);
-
-        graph.addEdge(node6, node7);
-        graph.addEdge(node7, node8);
-        graph.addEdge(node8, node20);
-        graph.addEdge(node8, node9);
-        graph.addEdge(node9, node10);
-        graph.addEdge(node10, node11);
-        graph.addEdge(node11, node12);
-        graph.addEdge(node12, node13);
-        graph.addEdge(node13, node14);
-        graph.addEdge(node14, node15);
-        graph.addEdge(node15, node16);
-        graph.addEdge(node16, node17);
-        graph.addEdge(node17, node18);
-        graph.addEdge(node18, node19);
-        graph.addEdge(node19, node20);
-
-        graph.addEdge(node10, node1);
-        graph.addEdge(node11, node2);
-        graph.addEdge(node12, node3);
-        graph.addEdge(node14, node4);
-        graph.addEdge(node15, node5);
-
-        sourceNode = node6;
-
-        //floor = new Floor("Gould Simpson : Floor 9 : ", rooms, graph);
         Bundle b2 = getIntent().getExtras();
         floor = db.getFloor(b2.getString("buildingName"), b2.getInt("floorNumber"));
         host.setCurrentTabByTag(b2.getInt("floorNumber") +"");
@@ -180,7 +108,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         if(event.getAction() == MotionEvent.ACTION_UP && Math.abs(x - downX) < 10 && Math.abs(y - downY) < 10) {
             selectRoom(x, y);
-            selectNode(x, y);
         }
         return false;
     }
@@ -189,7 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         Rect currentRect;
         boolean inRoom = false;
         for(DBRoom room : floor.getRooms()) {
-            if(room.hasCoordinates(x, y)) {
+            selectedNode = floor.findNode(x, y);
+            if(room.hasCoordinates(x, y) && selectedNode != null) {
                 inRoom = true;
                 currentRect = room.getRoomRect();
                 hView.setRect(currentRect.left, currentRect.top, currentRect.right, currentRect.bottom);
@@ -204,12 +132,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
-    public void selectNode(int x, int y) {
-        FloorNode selectedNode = floor.findNode( x, y);
-        if(selectedNode != null) {
-            Log.d("yo", "we found it");
-            setDestinationNode(selectedNode);
-        }
+    private void updateDestinationNode() {
+        setDestinationNode(selectedNode);
+    }
+
+    private void updateSourceNode() {
+        sourceNode = selectedNode;
     }
 
     public void setDestinationNode(FloorNode node) {
@@ -227,8 +155,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     * Author: Miranda Motsinger
     */
     private void showPopupWindow(DBRoom room) {
-        final int roomX = room.getCenterX();
-        final int roomY = room.getCenterY();
         int x = room.getCenterX() - 150 - hView.getScrollX();
         int y = room.getCenterY() - 50;
 
@@ -250,15 +176,14 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         routeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectNode(roomX, roomY);
-                setDestinationNode(destinationNode);
+                updateDestinationNode();
             }
         });
         Button startButton = (Button) pw.getContentView().findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sourceNode = floor.findNode(roomX, roomY);
+                updateSourceNode();
             }
         });
 
@@ -342,7 +267,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 Rect currentRect = room.getRoomRect();
                 hView.setRect(currentRect.left, currentRect.top, currentRect.right, currentRect.bottom);
                 hView.scrollTo(currentRect.left - 100, 0);
-                selectNode(room.getCenterX(), room.getCenterY());
                 showPopupWindow(room);
                 break;
             }
@@ -371,7 +295,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 Rect currentRect = selectedRoom.getRoomRect();
                 hView.setRect(currentRect.left, currentRect.top, currentRect.right, currentRect.bottom);
                 hView.scrollTo(currentRect.left - 100, 0);
-                selectNode(selectedRoom.getCenterX(), selectedRoom.getCenterY());
                 showPopupWindow(selectedRoom);
             }
         }
